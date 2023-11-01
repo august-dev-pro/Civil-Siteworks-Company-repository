934175ee6bac56e21e9838ef9904bc9e4af24507
august-dev-pro <augustinselete01@gmail.com>
2023-11-01 17:43:05 +0000

    Civil-Siteworks-Company commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menuIcon");
+  const menuList = document.getElementById("menuList");
+
+  menuIcon.addEventListener("click", function () {
+    menuList.classList.toggle("menu__list--open");
+  });
+});
+ */
